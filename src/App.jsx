3ff07dfb298c34c975887fb765fa69c163613bda
Ryan Okamuro <ryaniscool003@yahoo.com>
2019-02-22 11:05:42 -0800

    Create landing branch

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import StockLanding from '../src/components/StockHomePage/StockLanding'
+import './App.css'; 
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <StockLanding />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/StockHomePage/StockLanding.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StockLanding extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StockLanding;
